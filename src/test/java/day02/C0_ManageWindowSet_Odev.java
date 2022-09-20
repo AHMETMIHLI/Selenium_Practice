6ac842b8c97a321b9832af02d7a7ff1c7e510a39
Ayşe Erdem <aayseerdem0633@gmail.com>
2022-09-20 23:23:41 +0300

    20 Eylul

--- src/test/java/day02/C0_ManageWindowSet_Odev.java
+++ src/test/java/day02/C0_ManageWindowSet_Odev.java
@@ -3,7 +3,10 @@ package day02;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class C0_ManageWindowSet_Odev {
     public static void main(String[] args) {
@@ -19,22 +22,41 @@ public class C0_ManageWindowSet_Odev {
         System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
         WebDriver driver= new ChromeDriver();
 
-        //2-
+        //2-Amazon sayfasına gidelim. https://www.amazon.com/
         driver.get("https://www.amazon.com/");
 
-        //3-
+        //3-Sayfalanın konumunu ve boyutlarını yazdırın
         System.out.println("Sayfanın ilk konumu : "+ driver.manage().window().getPosition());
         System.out.println("Sayfanın ilk boyutu :"+ driver.manage().window().getSize());
 
-        //4-
-        driver.manage().window().setPosition(new Point(60,30));
-        driver.manage().window().setSize(new Dimension(800,600));
+        //4-Sayfanın konumunu ve boyutunu istediğiniz şekilde ayarlayın
+        int konumX=60;
+        int konumY=30;
+        driver.manage().window().setPosition(new Point(konumX,konumY));
+
+        int width=800;
+        int height=600;
+        driver.manage().window().setSize(new Dimension(width,height));
 
 
-        //5-
+        // 5- Sayfanın sizin istediğiniz konum ve boyuta geldiğini test edin
 
-        System.out.println("Sayfanın istenen koordinat konumu : "+ driver.manage().window().getPosition());
-        System.out.println("Sayfanın istenen boyut ölçüleri : "+ driver.manage().window().getSize());
+        //Konum testi
+        int x=driver.manage().window().getPosition().getX();
+        int y=driver.manage().window().getPosition().getY();
+        if(konumX==x && konumY==y){
+            System.out.println("İstenen Konum  testi PASSED");
+        }else System.out.println("İstenen Konum  testi FAILED");
+
+        // Boyut Testi
+        int widthSayi=driver.manage().window().getSize().getWidth();
+        int heightSayi=driver.manage().window().getSize().getHeight();
+        if(widthSayi==width && heightSayi==height){
+            System.out.println("İstenen Boyut  testi PASSED");
+        }else System.out.println("İstenen Boyut  testi FAILED");
+
+
+
 
         //6-
         driver.close();
